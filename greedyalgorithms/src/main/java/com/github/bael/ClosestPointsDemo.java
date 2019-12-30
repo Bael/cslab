eced9c9bad4429ba065e341d204d150533b46ab7
Denis <denkern@gmail.com>
2019-12-30 13:58:04 +0700

    code review task

--- greedyalgorithms/src/main/java/com/github/bael/ClosestPointsDemo.java
+++ greedyalgorithms/src/main/java/com/github/bael/ClosestPointsDemo.java
@@ -1,70 +1,47 @@
 package com.github.bael;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 
-import java.util.*;
-
+/**
+ * Класс для запуска алгоритма расчета поиска ближайших точек
+ */
 public class ClosestPointsDemo {
 
     public static void main(String[] args) {
 
         ClosestPointsDemo demo = new ClosestPointsDemo();
         Scanner sc = new Scanner(System.in);
-
-//        Point[] coordinates = demo.produceFromInput(sc);
-        Point[] coordinates = demo.produceFromRandom4();
-        double distance = new ClosestPointsNaive(coordinates).closestDistance();
-        System.out.println(distance);
-
+        Point[] coordinates = demo.readCoordinates(sc);
         double distanceDiv = new ClosestPointsDivideAndConquer(coordinates).closestDistance();
-        System.out.println(distanceDiv);
-
-//        double distanceDiv2 = new ClosestPointsDivideAndConquer(demo.produceFromRandom2()).closestDistance();
-//        System.out.println(Double.compare(distanceDiv2, 2.828427125));
-//        System.out.printf("%.4f\n", distanceDiv2);
-//        System.out.println(distanceDiv2);
-
-
-
+        System.out.printf("%.4f\n", distanceDiv);
     }
 
-    private static double fullCombinationDistance(Point[] points, int l, int r) {
-        if (l >= r) {
-            return 0;
-        }
+    /**
+     * Считываем координаты из пользовательского ввода
+     *
+     * @return массив точек
+     */
+    private Point[] readCoordinates(Scanner sc) {
+        Objects.requireNonNull(sc, "Не заполнен сканер чтения данных!");
+        // инициализируем массив координат
+        int count = sc.nextInt();
+        Point[] coordinates = new Point[count];
 
-        double minDistance = points[l].distance(points[l + 1]);
-        for (int i = l; i < points.length - 1 && i < r; i++) {
-            for (int j = i + 1; j < points.length && j < r; j++) {
-                double currentDistance = Point.distance(points[i], points[j]);
-                if (currentDistance < minDistance) {
-                    minDistance = currentDistance;
-                }
-            }
+        for (int i = 0; i < count; i++) {
+            long x = sc.nextLong();
+            long y = sc.nextLong();
+            coordinates[i] = new Point(x, y);
         }
-        return minDistance;
+        return coordinates;
     }
 
-    private Point[] produceFromRandom() {
-        Point[] points = new Point[10000];
-        Random a = new Random();
-        int boundX = 100000;
-        int boundY = 100000;
-        for (int i=0; i< points.length; i++) {
-            if (i % 7 == 0) {
-                points[i] = new Point(-a.nextInt(boundX), -a.nextInt(boundY));
-            } else {
-                if (i % 3 == 7) {
-                    points[i] = new Point(-a.nextInt(boundX), a.nextInt(boundY));
-                }
-                 else {
-                    points[i] = new Point(a.nextInt(boundX), a.nextInt(boundY));
-                }
-            }
-        }
-        return points;
-    }
-
-    private Point[] produceFromRandom3() {
+    // тестовый набор данных
+    private Point[] produceTestCoordinatesSet() {
         Point[] points = new Point[11];
         points[0] = new Point(4, 4);
         points[1] = new Point(-2, -2);
@@ -80,45 +57,12 @@ public class ClosestPointsDemo {
         return points;
     }
 
-    private Point[] produceFromRandom2() {
-        Point[] points = new Point[4];
-        points[0] = new Point(0, 0);
-        points[1] = new Point(5, 6);
-        points[2] = new Point(3, 4);
-        points[3] = new Point(7, 2);
-        return points;
-    }
-
-    private Point[] produceFromRandom4() {
-        Point[] points = new Point[5];
-//        Correct output:
-//        1.414213562
-
-        points[0] = new Point(1, -1);
-        points[1] = new Point(0, 4);
-        points[2] = new Point(-5, -1);
-        points[3] = new Point(2, 5);
-        points[4] = new Point(3, 4);
-        return points;
-    }
-
-
-
-    private Point[] produceFromInput(Scanner sc) {
-        Objects.requireNonNull(sc, "Не заполнен сканер чтения данных!");
-        // инициализируем массив координат
-        int count = sc.nextInt();
-        Point[] coordinates = new Point[count];
-
-        for (int i = 0; i < count; i++) {
-            long x = sc.nextLong();
-            long y = sc.nextLong();
-            coordinates[i] = new Point(x, y);
-        }
-        return coordinates;
-    }
-
+    /***
+     * Класс точки в двухмерном пространстве.
+     * Умеет вычислять дистанцию до другой точки.
+     */
     private static class Point {
+
         private long x;
         private long y;
 
@@ -151,7 +95,11 @@ public class ClosestPointsDemo {
         }
     }
 
+    /**
+     * Базовый класс алгоритма расчета. Задает контракт - метод возвращающий минимальную дистанцию.
+     */
     private static abstract class ClosestPoints {
+
         protected final Point[] points;
 
         public ClosestPoints(Point[] points) {
@@ -159,10 +107,39 @@ public class ClosestPointsDemo {
             this.points = points;
         }
 
+        /***
+         * Нахождение минимальной дистанции полным перебором
+         * (для проверки основного алгоритма на тестовом наборе данных)
+         * @param points - входные точки
+         * @param l - левая граница
+         * @param r - правая граница
+         * @return минимальная дистанция
+         */
+        protected static double fullCombinationDistance(Point[] points, int l, int r) {
+            if (l >= r) {
+                return 0;
+            }
+
+            double minDistance = points[l].distance(points[l + 1]);
+            for (int i = l; i < points.length - 1 && i < r; i++) {
+                for (int j = i + 1; j < points.length && j < r; j++) {
+                    double currentDistance = Point.distance(points[i], points[j]);
+                    if (currentDistance < minDistance) {
+                        minDistance = currentDistance;
+                    }
+                }
+            }
+            return minDistance;
+        }
+
         public abstract double closestDistance();
     }
 
+    /**
+     * Класс расчета простым перебором  (O(n^2))
+     */
     private static class ClosestPointsNaive extends ClosestPoints {
+
         public ClosestPointsNaive(Point[] points) {
             super(points);
         }
@@ -172,72 +149,74 @@ public class ClosestPointsDemo {
                 return 0;
             }
 
-            double minDistance = this.points[0].distance(this.points[1]);
-            for (int i = 0; i < this.points.length; i++) {
-                for (int j = i + 1; j < this.points.length; j++) {
-                    double currentDistance = Point.distance(points[i], points[j]);
-                    if (currentDistance < minDistance) {
-                        minDistance = currentDistance;
-                    }
-                }
-            }
-            return minDistance;
+            return fullCombinationDistance(points, 0, points.length - 1);
         }
     }
 
+    /***
+     * Класс расчета методом разделяй и властвуй, описанным в задании.
+     */
     private static class ClosestPointsDivideAndConquer extends ClosestPoints {
+
         public ClosestPointsDivideAndConquer(Point[] points) {
             super(points);
         }
 
         public double closestDistance() {
             Arrays.sort(points, Comparator.comparingLong(o -> o.x));
-            return divide(points, 0, points.length - 1);
+            return closestDistance(points, 0, points.length - 1);
         }
 
-        private double divide(Point[] points, int l, int r) {
-//            System.out.printf("Divide called with points %s and l %s and r %s \n",
-//                    Arrays.toString(points), l, r);
+        /***
+         * Внутренний метод расчета минимального расстояния по поданному массиву точек и правой и левой границах,
+         * внутри которых осуществляется поиск.
+         * @param points массив входных точек
+         * @param l левая граница для поиска
+         * @param r правая граница для поиска
+         * @return минимальное расстояние
+         */
+        private double closestDistance(Point[] points, int l, int r) {
+            // сколько точек подряд проверяем по оси Y?
+            int ySequenceCount = 7;
+            //
             if (r - l > 3) {
                 int m = (r + l) / 2;
-                double leftDistance = divide(points, l, m);
-                double rightDistance = divide(points, m + 1, r);
+                double leftDistance = closestDistance(points, l, m);
+                double rightDistance = closestDistance(points, m + 1, r);
                 double minD = Math.min(leftDistance, rightDistance);
 
-                // find range:
+                // Набор точек для сортировки по y координате
                 List<Point> yList = new ArrayList<>();
                 int rightX = m;
                 double x = points[m].x;
-                // отбираем все точки справа
-                while (rightX < r && (points[rightX].x - x < minD)) {
+
+                // отбираем все точки справа для набора в сортировку по y координате
+                while (rightX <= r && (points[rightX].x - x < minD)) {
                     yList.add(points[rightX++]);
                 }
 
-                // отбираем все точки слева
+                // отбираем все точки слева для набора в сортировку по y координате
                 int leftX = m - 1;
-                while (leftX > l && (x - points[leftX].x < minD)) {
+                while (leftX >= l && (x - points[leftX].x < minD)) {
                     yList.add(points[leftX--]);
                 }
                 yList.sort(Comparator.comparingLong(o -> o.y));
                 double minAdjPoint = minD;
-
-//                Point[] yArray = yList.toArray(new Point[0]);
+                // по отсортированным по координате Y точкам ищем минимум расстояния среди
+                // подмножества 7 ближайших точек к обрабатываемой в данный момент точке.
                 for (int i = 0; i < yList.size() - 1; i++) {
                     Point p = yList.get(i);
-                    for (int j = i + 1; j < yList.size() && j < i + 7; j++) {
+                    for (int j = i + 1; j < yList.size()
+                            && yList.get(j).y - yList.get(i).y < minAdjPoint
+                            && j < i + ySequenceCount; j++) {
                         Point adjPoint = yList.get(j);
                         minAdjPoint = Math.min(minAdjPoint, p.distance(adjPoint));
                     }
                 }
-
                 return minAdjPoint;
             } else {
-//                System.out.println("Closed recursion!");
                 return fullCombinationDistance(points, l, r);
             }
-
         }
     }
-
-
 }
